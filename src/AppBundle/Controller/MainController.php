51a4c38471ff688cc0c6ea1b5b18f413ebcf0c6d
Pierre Portejoie <portejoiepierre@gmail.com>
2016-09-02 19:07:40 +0200

    (minor) css changes

--- src/AppBundle/Controller/MainController.php
+++ src/AppBundle/Controller/MainController.php
@@ -73,7 +73,32 @@ class MainController extends Controller{
         $cat1 = array(
             'name' => 'coworking',
             'displayName' => 'Coworking',
-            'pitch' => 'hitecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolore'
+            'pitch' => 'Trouvez les espaces de coworking les plus appréciés des entrepreneurs dans votre secteur.'
+        );
+        $cat2 = array(
+            'name' => 'comptabilite',
+            'displayName' => 'Comptabilité',
+            'pitch' => 'TTrouvez la personne qu’il vous faut pour cajoler vos finances'
+        );
+        $cat3 = array(
+            'name' => 'autres',
+            'displayName' => 'Prototypage',
+            'pitch' => 'Réalisez un exemplaire de votre produit, qu’il soit électronique, plastique, mécanique, ...'
+        );
+        $cat4 = array(
+            'name' => 'juridique',
+            'displayName' => 'Juridique',
+            'pitch' => 'Trouvez l’expert qui éclairera enfin vos démarches juridiques avec ses conseils bienvenus.'
+        );
+        $cat5 = array(
+            'name' => 'marketing',
+            'displayName' => 'Marketing',
+            'pitch' => 'Exister online ou offline n’est pas du tout évident ! Place aux professionnels du Marketing.'
+        );
+        $cat6 = array(
+            'name' => 'graphisme',
+            'displayName' => 'Web Design',
+            'pitch' => 'Trouvez ici les développeurs, designers, et graphistes les plus aimés des entrepreneurs.'
         );
 
         //DUMMY FORUM POST
@@ -81,14 +106,38 @@ class MainController extends Controller{
             'title' => 'Titre de la conversation',
             'date' => '01/01/2017',
             'name' => 'Jean Dujardin',
-            'photoUrl' => 'images/bigard.png',
-            'content' => "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.",
+            'photoUrl' => 'images/images_forum/face-1.png',
+            'content' => "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. ",
+        );
+        $post2 = array(
+            'title' => 'Titre de la conversation',
+            'date' => '01/01/2017',
+            'name' => 'Jean Dujardin',
+            'photoUrl' => 'images/images_forum/face-2.png',
+            'content' => "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. ",
+        );
+        $post3 = array(
+            'title' => 'Titre de la conversation',
+            'date' => '01/01/2017',
+            'name' => 'Jean Dujardin',
+            'photoUrl' => 'images/images_forum/face-3.png',
+            'content' => "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. ",
         );
 
         //DUMMY TESTIMONIAL
         $testimonial1 = array(
             'name' => 'Patrick Sébastien',
-            'photoUrl' => 'images/bigard.png',
+            'photoUrl' => 'images/images_forum/face-1.png',
+            'content' => "On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who "
+        );
+        $testimonial2 = array(
+            'name' => 'Patrick Sébastien',
+            'photoUrl' => 'images/images_forum/face-2.png',
+            'content' => "On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who "
+        );
+        $testimonial3 = array(
+            'name' => 'Patrick Sébastien',
+            'photoUrl' => 'images/images_forum/face-3.png',
             'content' => "On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who "
         );
 
@@ -98,9 +147,9 @@ class MainController extends Controller{
             'messages' => rand(0,9)
         );
         $listing_extracts = array($service1, $service1, $service1);
-        $categories = array($cat1, $cat1, $cat1, $cat1);
-        $forum_posts = array($post1, $post1, $post1);
-        $testimonials = array($testimonial1, $testimonial1, $testimonial1);
+        $categories = array($cat1, $cat2, $cat3, $cat4, $cat5, $cat6);
+        $forum_posts = array($post1, $post2, $post3);
+        $testimonials = array($testimonial1, $testimonial2, $testimonial3);
 
         return $this->render('V2/index.html.twig',[
             'title' => 'Entreprenons ensemble | Upsters',
