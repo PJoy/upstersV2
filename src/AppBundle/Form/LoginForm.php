ffc2a3ed94cb7736985e571008320af9d780f446
Pierre Portejoie <portejoiepierre@gmail.com>
2016-09-11 15:20:16 +0200

    (minor) front login ok but not working

--- src/AppBundle/Form/LoginForm.php
+++ src/AppBundle/Form/LoginForm.php
@@ -4,6 +4,7 @@ namespace AppBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,8 +14,12 @@ class LoginForm extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('_username')
-            ->add('_password', PasswordType::class);
+            ->add('_username', TextType::class, array(
+                'label' => 'Adresse e-mail'
+            ))
+            ->add('_password', PasswordType::class, array(
+                'label' => 'Mot de passe'
+            ));
     }
 
 }
