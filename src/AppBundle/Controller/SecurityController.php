d18450d3fe43169edc0d92ef1b82347b5adfff85
Pierre Portejoie <portejoiepierre@gmail.com>
2016-09-19 19:40:53 +0200

    (minor) fix login full page

--- src/AppBundle/Controller/SecurityController.php
+++ src/AppBundle/Controller/SecurityController.php
@@ -42,6 +42,30 @@ class SecurityController extends Controller
     }
 
     /**
+     * @Route("/login-full", name="login")
+     */
+    public function fullPageLoginAction(){
+        $authenticationUtils = $this->get('security.authentication_utils');
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        $form = $this->createForm(LoginForm::class, [
+            '_username' => $lastUsername
+        ]);
+
+        return $this->render(
+            'user/loginFullPage.html.twig',
+            array(
+                // last username entered by the user
+                'form' => $form->createView(),
+                'error'         => $error,
+            )
+        );
+    }
+
+    /**
      * @Route("/logout", name="security_logout")
      */
     public function logoutAction()
