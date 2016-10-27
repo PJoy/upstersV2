ba94cf47abda955f2cbe3609faf9e666c7c553c1
Pierre Portejoie <portejoiepierre@gmail.com>
2016-10-27 18:08:46 +0200

    text fixes

--- src/AppBundle/Form/ResourceFormType.php
+++ src/AppBundle/Form/ResourceFormType.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,7 +13,9 @@ class ResourceFormType extends AbstractType
     {
         $builder
             ->add('name')
-            ->add('category')
+            ->add('category', TextType::class, [
+                'label' => 'Métier'
+            ])
             ->add('company')
             ->add('address')
             ->add('GPSLat')
--- src/AppBundle/Form/UserEditForm.php
+++ src/AppBundle/Form/UserEditForm.php
@@ -7,6 +7,7 @@ use Symfony\Component\DomCrawler\Field\ChoiceFormField;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
@@ -39,7 +40,9 @@ class UserEditForm extends AbstractType
                 'multiple' => true,
                 'expanded' => true
             ))*/
-            ->add('birthDate')
+            ->add('birthDate', DateType::class, [
+                'widget' => 'text'
+            ])
             ->add('pitch')
             ->add('image', FileType::class, array(
                 'data_class' => null,
